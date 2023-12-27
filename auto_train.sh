8fba09a59a3e85c61a7c446ae7b227d447bcd390
JingdiZhou <1031015308@qq.com>
2023-12-27 15:06:08 +0800

    add auto_train.sh which can train different env and algo automatically

--- auto_train.sh
+++ auto_train.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 set -e
 
-Learning_rate = (0.0001 0.0005 0.001 0.005 0.01 0.05 0.1 0.5)
-Rho = (0.01 0.02 0.05 0.1 0.2 0.5)
-#echo ${Learning_rate[@]}
+Learning_rate=(0.0001 0.0005 0.001 0.005 0.01 0.05 0.1 0.5)
+Rho=(0.01 0.02 0.05 0.1 0.2 0.5)
+echo "Grid search of Learning rate:[${Learning_rate[*]}]"
+echo "Grid search of rho:[${Rho[*]}]"
 for lr in ${Learning_rate[*]};do
   for rho in ${Rho[*]};do
-    python train.py --algo $1 --env $2 --device cuda --optimize-choice $3 --quantize $4 -P --rho rho -params learning_rate:lr
+    for ((i=1;i<=$5;i++)) # Test different random seeds, $5 can be set random number, but for fairness, it should be set big enough
+    do
+    echo "Test learning_rate: $lr, rho: $rho"
+    python train.py --algo $1 --env $2 --device cuda --optimize-choice $3 --quantize $4 -P --rho $rho -params learning_rate:$lr --track -n $6
+    done
 done
 done
